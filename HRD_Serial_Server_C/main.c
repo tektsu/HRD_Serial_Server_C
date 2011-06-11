32e2f10dc10da1d9ad6a476a120b4beb8c7eb2f0
Steve Baker <steve@tandika.com>
2011-06-10 18:00:11 -0700

    Initial Commit

---
+++ HRD_Serial_Server_C/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  HRD_Serial_Server_C
+//
+//  Created by Steve Baker on 6/10/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+  // insert code here...
+  printf("Hello, World!\n");
+    return 0;
+}
+
